b68f5d8a91bd3f12a0de73aa2f6e05f9703bb1f5
Ytstudio <ytcreatorstudio2001@gmail.com>
2023-01-24 21:54:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ app = Flask(__name__)
 def hello_world():
    return 'LazyDeveloper'
 
-os.system("git clone https://LazyDeveloperr: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@github.com/LazyDeveloperr/Lazyv2testbot okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 bot.py &")
+os.system("git clone https://ytcreatorstudio2001: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@github.com/ytcreatorstudio2001/LazyPrincessV2 okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 bot.py &")
